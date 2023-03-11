10a4d8666988242adfaa9b80cc5a6e1248aef8fb
L. Dikan <allfromleon@gmail.com>
2023-03-11 18:42:25 +0100

    optimiert Reha Page

--- src/landing/Rehabilitation.js
+++ src/landing/Rehabilitation.js
@@ -40,7 +40,7 @@ export default function Rehabilitation() {
                             <div className="absolute inset-x-0 bottom-0">
                                 <div className="m-10 pb-10">
                                     <div className="grid grid-cols-1">
-                                        <div className="bg-white rounded-lg p-10 shadow-lg bg-opacity-80">
+                                        <div className="bg-white rounded-lg p-10 shadow-lg bg-opacity-80 border-gray-500 border-2">
                                             <h1 className="text-2xl border-b-2 border-black mb-5">Rehabilitation</h1>
                                             <p className="text-justify mb-5">Die christliche Rehabilitation "Betesda" ist ein ganzheitlicher Ansatz zur Wiedereingliederung von straffällig gewordenen Personen in die Gesellschaft. Es geht dabei nicht nur um die Unterstützung bei der Bewältigung von Suchtproblemen oder der Vermittlung von Arbeit, sondern auch um die Förderung des Glaubens und der Fürsorge für Strafgefangene und ehemalige Strafgefangene.</p>
                                             <p className="text-justify mb-5">Eine zentrale Rolle spielt dabei die Bereitstellung von Unterkunft und Verpflegung in Form von Wohngemeinschaften. Hier können die Bewohner von regelmäßiger Seelsorge und Arbeitstherapie profitieren, um ihre psychischen und emotionalen Probleme zu bewältigen und eine positive Einstellung zu entwickeln.</p>
@@ -54,7 +54,7 @@ export default function Rehabilitation() {
                 </Animator>
             </ScrollPage>
             <ScrollPage>
-                <Animator animation={batch(MoveIn(-3000,0), Sticky(), MoveOut(1000, 0))}>
+                <Animator animation={batch(MoveIn(-3000,0), Sticky(), MoveOut(3000, 0))}>
                     <div className="w-screen">
                         <div className="relative">
                             <img src={BuildingView} className=""></img>
@@ -63,7 +63,7 @@ export default function Rehabilitation() {
                                     <div className="grid grid-cols-3 gap-4">
                                         <div></div>
                                         <div></div>
-                                        <div className="bg-slate-50 rounded-lg p-10 shadow-lg">
+                                        <div className="bg-slate-50 rounded-lg p-10 shadow-lg border-gray-500 border-2">
                                             <p className="text-justify mb-3">Darüber hinaus wird auch präventive Hilfe durch geeignete Angebote der Jugend- und Bildungsarbeit angeboten. Hierdurch sollen junge Menschen in schwierigen sozialen Situationen unterstützt und präventive Maßnahmen gegen Kriminalität und Sucht ergriffen werden. Insgesamt bietet die christliche Rehabilitation einen ganzheitlichen Ansatz zur Wiederherstellung von individuellem Wohlbefinden und sozialer Integration.</p>
                                         </div>
                                     </div>
